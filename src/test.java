1536bb047ea7b5778862e1e5a392a1881ee201f8
HoangNguyen <h01694111033@gmail.com>
2019-05-16 08:37:23 +0700

    a

---
+++ src/test.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class test {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a:");
+        double a = scanner.nextDouble();
+        System.out.println("Enter b:");
+        double b = scanner.nextDouble();
+        System.out.println("Enter c:");
+        double c = scanner.nextDouble();
+        QuadraticEquation quadraticEquation = new QuadraticEquation(a,b,c);
+        if (quadraticEquation.getDiscriminant()<0){
+            System.out.println("The equation has no real root");
+        } else if (quadraticEquation.getDiscriminant() == 0){
+            System.out.println("The equation has one root is " +quadraticEquation.getRoot1());
+        } else {
+            System.out.println("The equation has two roots are "+quadraticEquation.getRoot1()+ " and "+quadraticEquation.getRoot2());
+        }
+    }
+}
